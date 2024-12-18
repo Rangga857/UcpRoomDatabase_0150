c55f68d5f7e7b98be4b268302d482c984b3d82f3
Rangga857 <azharrangga28@gmail.com>
2024-12-18 08:46:26 +0700

    membuat MkDsDatabase untuk konfigurasi Room database

---
+++ app/src/main/java/com/example/ucpii_pam/data/database/MkDsDatabase.kt
@@ -0,0 +1,34 @@
+package com.example.ucpii_pam.data.database
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.example.ucpii_pam.data.dao.DosenDao
+import com.example.ucpii_pam.data.dao.MataKuliahDao
+import com.example.ucpii_pam.data.entity.Dosen
+import com.example.ucpii_pam.data.entity.MataKuliah
+
+@Database(entities = [MataKuliah::class], [Dosen::class],version = 1, exportSchema =  false)
+abstract class MkDsDatabase : RoomDatabase() {
+
+    abstract fun matakuliahDao(): MataKuliahDao
+    abstract fun dosenDao(): DosenDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: MkDsDatabase? = null
+
+        fun getDatabase(context: Context): MkDsDatabase {
+            return (INSTANCE ?: synchronized(this) {
+                Room.databaseBuilder(
+                    context,
+                    MkDsDatabase::class.java,
+                    "MkDsDatabase"
+                )
+                    .build().also { INSTANCE = it }
+            }
+                    )
+        }
+    }
+}
