376d91b498adf51fa143a2d05550d245a4ae3b4b
Rangga857 <azharrangga28@gmail.com>
2024-12-19 11:23:13 +0700

    Menambahkan anotasi @Dao dan menggunakan Flow untuk query MataKuliah

--- app/src/main/java/com/example/ucpii_pam/data/dao/MataKuliahDao.kt
+++ app/src/main/java/com/example/ucpii_pam/data/dao/MataKuliahDao.kt
@@ -1,17 +1,20 @@
 package com.example.ucpii_pam.data.dao
 
+import androidx.room.Dao
 import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.Query
 import androidx.room.Update
 import com.example.ucpii_pam.data.entity.MataKuliah
+import kotlinx.coroutines.flow.Flow
 
+@Dao
 interface MataKuliahDao {
     @Query("SELECT * FROM matakuliah")
-    fun getAllmatakuliah(): List<MataKuliah>
+    fun getAllmatakuliah(): Flow<List<MataKuliah>>
 
     @Query("SELECT * FROM matakuliah WHERE kodeMk = :kodeMk")
-    fun getByIdmatakuliah(kodeMk: String): MataKuliah
+    fun getByIdmatakuliah(kodeMk: String): Flow<MataKuliah>
 
     @Insert
     suspend fun insertmatakuliah(matakuliah: MataKuliah)
