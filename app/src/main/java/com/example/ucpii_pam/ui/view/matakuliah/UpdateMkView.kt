b4ddaf862c213aaeb781a5b8348f2a9d844493bc
Rangga857 <azharrangga28@gmail.com>
2024-12-22 00:25:37 +0700

    Menambahkan Scaffold dengan TopAppBar dan BottomBar pada UpdateMhsView

--- app/src/main/java/com/example/ucpii_pam/ui/view/matakuliah/UpdateMkView.kt
+++ app/src/main/java/com/example/ucpii_pam/ui/view/matakuliah/UpdateMkView.kt
@@ -1,16 +1,27 @@
 package com.example.ucpii_pam.ui.view.matakuliah
 
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarDuration
+import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.ucpii_pam.ui.costumwidget.TopAppBar
 import com.example.ucpii_pam.ui.viewmodel.matakuliah.PenyediaMkViewModel
 import com.example.ucpii_pam.ui.viewmodel.matakuliah.UpdateViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 @Composable
 fun UpdateMhsView(
@@ -38,5 +49,37 @@ fun UpdateMhsView(
             }
         }
     }
-
+    Scaffold (
+        modifier=modifier,
+        snackbarHost = { SnackbarHost(hostState = snackbarHostState) },
+        topBar = {
+            TopAppBar(
+                judul = "Edit MataKuliah",
+                showBackButton = true,
+                onBack = onBack,
+                modifier = modifier
+            )
+        },
+        bottomBar = {
+            Button(
+                onClick = {
+                    coroutineScope.launch {
+                        if (viewModel.validateFields()) {
+                            viewModel.updateData()
+                            delay(500)
+                            withContext(Dispatchers.Main) {
+                                onNavigate()
+                            }
+                        }
+                    }
+                },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(start = 50.dp, end = 50.dp)
+            ) {
+                Text("Simpan")
+            }
+        }
+    ){
+    }
 }
