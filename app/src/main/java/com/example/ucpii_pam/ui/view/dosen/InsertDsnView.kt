998d69de7410b22e0a648cbd2065d23d219fce0e
Rangga857 <azharrangga28@gmail.com>
2024-12-21 14:52:05 +0700

    Create fungsi InsertBodyDsn sebagai body tampilan

--- app/src/main/java/com/example/ucpii_pam/ui/view/dosen/InsertDsnView.kt
+++ app/src/main/java/com/example/ucpii_pam/ui/view/dosen/InsertDsnView.kt
@@ -1,12 +1,17 @@
 package com.example.ucpii_pam.ui.view.dosen
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Text
@@ -17,7 +22,39 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.ucpii_pam.ui.viewmodel.dosen.DosenEvent
+import com.example.ucpii_pam.ui.viewmodel.dosen.DosenUiState
 import com.example.ucpii_pam.ui.viewmodel.dosen.FormErrorState
+
+@Composable
+fun InsertBodyDsn(
+    modifier: Modifier = Modifier,
+    onValueChange: (DosenEvent) -> Unit = {},
+    dsnuiState: DosenUiState,
+    onClick: ()-> Unit
+){
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(Color.White)
+            .border(2.dp, Color.Gray, RoundedCornerShape(16.dp))
+            .padding(16.dp)
+    ) {
+        FormDosen(
+            dosenEvent = dsnuiState.dosenEvent,
+            onValueChange = onValueChange,
+            errorState = dsnuiState.isEntryValid,
+            modifier = Modifier.fillMaxWidth()
+        )
+    }
+    Spacer(modifier = modifier.padding(10.dp))
+    Button(
+        onClick = onClick,
+        modifier = Modifier.fillMaxWidth()
+    ) {
+        Text(text = "Simpan")
+    }
+
+}
 
 @Composable
 fun FormDosen(
