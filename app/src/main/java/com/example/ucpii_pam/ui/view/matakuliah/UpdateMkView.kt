8e0a6e2cc15fefc2de2787d2d30e5163f7ef45d4
Rangga857 <azharrangga28@gmail.com>
2024-12-22 00:20:39 +0700

    deklarasi variabel uimkState, snackbarHostState, coeoutineScope

--- app/src/main/java/com/example/ucpii_pam/ui/view/matakuliah/UpdateMkView.kt
+++ app/src/main/java/com/example/ucpii_pam/ui/view/matakuliah/UpdateMkView.kt
@@ -1,6 +1,9 @@
 package com.example.ucpii_pam.ui.view.matakuliah
 
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.ucpii_pam.ui.viewmodel.matakuliah.PenyediaMkViewModel
@@ -14,5 +17,8 @@ fun UpdateMhsView(
     viewModel: UpdateViewModel = viewModel(factory = PenyediaMkViewModel.Factory)
 
 ){
+    val uimkState = viewModel.updatemkUIState
+    val snackbarHostState = remember { SnackbarHostState() }
+    val coroutineScope = rememberCoroutineScope()
 
 }
