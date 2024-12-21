8b28a2983ae271ecadb2369379ae227555918d0b
Rangga857 <azharrangga28@gmail.com>
2024-12-21 21:01:01 +0700

    Create 2 data clas(MKuiState sebagai penyimpan state utama dan FormErrorState sebagai penyimpan status error)

--- app/src/main/java/com/example/ucpii_pam/ui/viewmodel/matakuliah/MkViewModel.kt
+++ app/src/main/java/com/example/ucpii_pam/ui/viewmodel/matakuliah/MkViewModel.kt
@@ -1,6 +1,28 @@
 package com.example.ucpii_pam.ui.viewmodel.matakuliah
 
+import com.example.ucpii_pam.data.entity.Dosen
 import com.example.ucpii_pam.data.entity.MataKuliah
+data class MKUiState(
+    val mataKuliahEvent: MataKuliahEvent = MataKuliahEvent(),
+    val isEntryValid: FormErrorState = FormErrorState(),
+    val snackbarMessage: String? = null,
+    val dosenOptions: List<Dosen> = emptyList()
+)
+
+data class FormErrorState(
+    val kodeMk: String? = null,
+    val nama: String? = null,
+    val sks: String? = null,
+    val semester: String? = null,
+    val jenis: String? = null,
+    val dosen_pengampu: String? = null,
+) {
+    fun isFormValid(): Boolean {
+        return kodeMk == null && nama == null
+                && sks == null && semester == null
+                && jenis == null && dosen_pengampu == null
+    }
+}
 
 fun MataKuliahEvent.toMataKuliahEntity():MataKuliah = MataKuliah(
     kodeMk = kodeMk,
