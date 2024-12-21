92d7d5e959dd2f449728e1faf8987b0daf432506
Rangga857 <azharrangga28@gmail.com>
2024-12-21 21:08:10 +0700

    Inisialisasi state dan mengambil daftar Dosen di MataKuliahViewModel

--- app/src/main/java/com/example/ucpii_pam/ui/viewmodel/matakuliah/MkViewModel.kt
+++ app/src/main/java/com/example/ucpii_pam/ui/viewmodel/matakuliah/MkViewModel.kt
@@ -1,14 +1,34 @@
 package com.example.ucpii_pam.ui.viewmodel.matakuliah
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.ucpii_pam.data.entity.Dosen
 import com.example.ucpii_pam.data.entity.MataKuliah
 import com.example.ucpii_pam.repository.RepositoryDsn
 import com.example.ucpii_pam.repository.RepositoryMK
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 
 class MataKuliahViewModel(
     private val repositoryMk: RepositoryMK,
     private val repositoryDsn: RepositoryDsn): ViewModel(){
+
+    var mkUiState by mutableStateOf(MKUiState())
+    val listDosen = mutableStateListOf<Dosen>()
+
+    init {
+        viewModelScope.launch {
+            repositoryDsn.getAllDosen().collectLatest { dosenList ->
+                listDosen.clear()
+                listDosen.addAll(dosenList)
+                mkUiState = mkUiState.copy(dosenOptions = dosenList)
+            }
+        }
+    }
 
 }
 
