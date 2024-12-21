340846b6217a5324abb00306ad14a496033f6961
Rangga857 <azharrangga28@gmail.com>
2024-12-22 00:29:17 +0700

    Create function PengelolaHalaman untuk mengatur navigasi dengan NavHostController

---
+++ app/src/main/java/com/example/ucpii_pam/ui/navigation/PengelolaHalaman.kt
@@ -0,0 +1,21 @@
+package com.example.ucpii_pam.ui.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.rememberNavController
+
+
+@Composable
+fun PengelolaHalaman(
+    navController: NavHostController = rememberNavController(),
+    modifier: Modifier = Modifier
+){
+    NavHost(
+        navController = navController,
+        startDestination = DestinasiHalamanUtama.route,
+    ) {
+    }
+
+}
