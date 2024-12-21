ed80221801fc30bb90b90b57d4e07a2b60e2d52d
Rangga857 <azharrangga28@gmail.com>
2024-12-21 17:37:50 +0700

    Create function BodyHomeDosenView untuk menampilkan konten daftar dosen dengan loading dan error handling

--- app/src/main/java/com/example/ucpii_pam/ui/view/dosen/HomeDsnView.kt
+++ app/src/main/java/com/example/ucpii_pam/ui/view/dosen/HomeDsnView.kt
@@ -1,8 +1,10 @@
 package com.example.ucpii_pam.ui.view.dosen
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
@@ -14,13 +16,58 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.filled.Info
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.ucpii_pam.data.entity.Dosen
+import com.example.ucpii_pam.ui.viewmodel.dosen.HomeUiState
+import kotlinx.coroutines.launch
+
+
+@Composable
+fun BodyHomeDosenView(
+    homeUiState: HomeUiState,
+    modifier: Modifier = Modifier
+){
+    val coroutineScope = rememberCoroutineScope()
+    val snackbarHostState = remember { SnackbarHostState() }
+    when{
+        homeUiState.isloading ->
+        {
+            Box(
+                modifier = modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ){
+                CircularProgressIndicator()
+            }
+        }
+        homeUiState.isError -> {
+            LaunchedEffect(homeUiState.errorMessage) {
+                homeUiState.errorMessage?.let{
+                    message ->
+                    coroutineScope.launch {
+                        snackbarHostState.showSnackbar(message)
+                    }
+                }
+            }
+        }
+        else -> {
+            ListDosen(
+                listDsn = homeUiState.dosenList,
+                modifier=modifier
+            )
+        }
+
+    }
+}
 
 @Composable
 fun ListDosen(
