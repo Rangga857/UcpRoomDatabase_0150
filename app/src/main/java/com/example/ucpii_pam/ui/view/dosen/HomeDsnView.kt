7d58ef1b83d65e8eb4224464d658c68842f19089
Rangga857 <azharrangga28@gmail.com>
2024-12-21 17:35:33 +0700

    Create function ListDosen untuk menampilkan list dosen menggunakan LazyColumn

--- app/src/main/java/com/example/ucpii_pam/ui/view/dosen/HomeDsnView.kt
+++ app/src/main/java/com/example/ucpii_pam/ui/view/dosen/HomeDsnView.kt
@@ -5,12 +5,14 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountBox
 import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.filled.Info
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -19,6 +21,26 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.ucpii_pam.data.entity.Dosen
+
+@Composable
+fun ListDosen(
+    listDsn: List<Dosen>,
+    modifier: Modifier = Modifier,
+){
+    LazyColumn (
+        modifier = modifier
+    ){
+        items(
+            items = listDsn,
+            itemContent = {dsn ->
+                CardDsn(
+                    dsn = dsn
+                )
+            }
+        )
+    }
+}
+
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
