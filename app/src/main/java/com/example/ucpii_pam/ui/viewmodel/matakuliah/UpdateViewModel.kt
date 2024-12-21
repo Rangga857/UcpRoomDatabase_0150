815f840e61ff81fd2533dc090684ea22d25f3f41
Rangga857 <azharrangga28@gmail.com>
2024-12-21 22:08:56 +0700

    Menambahkan fungsi untuk validasi, update data, dan menangani pesan snackbar pada UpdateViewModel

--- app/src/main/java/com/example/ucpii_pam/ui/viewmodel/matakuliah/UpdateViewModel.kt
+++ app/src/main/java/com/example/ucpii_pam/ui/viewmodel/matakuliah/UpdateViewModel.kt
@@ -36,4 +36,57 @@ class UpdateViewModel (
         }
     }
 
+    fun updatemkState(mataKuliahEvent: MataKuliahEvent){
+        updatemkUIState = updatemkUIState.copy(
+            mataKuliahEvent = mataKuliahEvent
+        )
+    }
+
+    fun validateFields() : Boolean{
+        val event = updatemkUIState.mataKuliahEvent
+        val errorState = FormErrorState(
+            kodeMk = if (event.kodeMk.isNotEmpty()) null else "Kode MK tidak boleh kosong",
+            nama = if (event.nama.isNotEmpty()) null else "Nama tidak boleh kosong",
+            sks = if (event.sks.isNotEmpty()) null else "SKS tidak boleh kosong",
+            semester = if (event.semester.isNotEmpty()) null else "Semester tidak boleh kosong",
+            jenis = if (event.jenis.isNotEmpty()) null else "Jenis tidak boleh kosong",
+            dosen_pengampu = if (event.dosen_pengampu.isNotEmpty()) null else "Dosen Pengampu tidak boleh kosong"
+        )
+
+        updatemkUIState = updatemkUIState.copy(isEntryValid = errorState)
+        return errorState.isFormValid()
+    }
+
+    fun updateData(){
+        val currentEvent = updatemkUIState.mataKuliahEvent
+        if (validateFields()){
+            viewModelScope.launch {
+                try {
+                    repositoryMK.updatematakuliah(currentEvent.toMataKuliahEntity())
+                    updatemkUIState = updatemkUIState.copy(
+                        snackbarMessage = "Data Berhasil Diupdate",
+                        mataKuliahEvent = MataKuliahEvent(),
+                        isEntryValid = FormErrorState()
+                    )
+                    println("snackbarMessage diatur: ${updatemkUIState.snackbarMessage}")
+                }
+                catch (e: Exception){
+                    updatemkUIState = updatemkUIState.copy(
+                        snackbarMessage = "Data gagal diupdate"
+                    )
+                }
+            }
+        }
+        else{
+            updatemkUIState = updatemkUIState.copy(
+                snackbarMessage = "Data gagal diupdate"
+            )
+        }
+    }
+    fun resetSnackBarMessage(){
+        updatemkUIState = updatemkUIState.copy(
+            snackbarMessage = null
+        )
+    }
+
 }
