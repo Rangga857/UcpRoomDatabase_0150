c3027e77d9758701ebac4eeb80cc4cd041957cbb
Rangga857 <azharrangga28@gmail.com>
2024-12-21 23:33:01 +0700

    Create BodyHomeMkView dengan snackbar, coroutineScope, dan onClick

--- app/src/main/java/com/example/ucpii_pam/ui/view/matakuliah/HomeMkView.kt
+++ app/src/main/java/com/example/ucpii_pam/ui/view/matakuliah/HomeMkView.kt
@@ -14,14 +14,30 @@ import androidx.compose.material.icons.filled.Person
 import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.example.ucpii_pam.data.entity.MataKuliah
 import androidx.compose.material3.Text
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.ucpii_pam.ui.viewmodel.matakuliah.HomeMkUiState
+
+@Composable
+fun BodyHomeMkView(
+    homeMkUiState: HomeMkUiState,
+    onClick: (String) -> Unit = { },
+    modifier: Modifier = Modifier
+){
+    val coroutineScope = rememberCoroutineScope()
+    val snackbarHostState = remember { SnackbarHostState() }
+
+
+}
 
 @Composable
 fun ListMataKuliah(
