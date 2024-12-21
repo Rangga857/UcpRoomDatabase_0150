2b87316bb35c84bd5737741ea287f7b354d67a1f
Rangga857 <azharrangga28@gmail.com>
2024-12-21 22:32:30 +0700

    Create fungsi CreationExtras.MkDsApp untuk akses instance MkDsApp

--- app/src/main/java/com/example/ucpii_pam/ui/viewmodel/matakuliah/PenyediaMkViewModel.kt
+++ app/src/main/java/com/example/ucpii_pam/ui/viewmodel/matakuliah/PenyediaMkViewModel.kt
@@ -1,6 +1,8 @@
 package com.example.ucpii_pam.ui.viewmodel.matakuliah
 
+import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.createSavedStateHandle
+import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.ucpii_pam.MkDsApp
@@ -34,3 +36,6 @@ object PenyediaMkViewModel{
         }
     }
 }
+
+fun CreationExtras.MkDsApp():MkDsApp =
+    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as MkDsApp)
